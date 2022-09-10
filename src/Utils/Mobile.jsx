efc36cac1a8b0e7db83cbbe8d1d53d1082a8a9fc
AJAY CHAWARIYA <Ajaychawariya34@gmail.com>
2022-09-10 19:30:57 +0530

    Navbar Added

--- src/Utils/Mobile.jsx
+++ src/Utils/Mobile.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
-const Mobile = () => {
+import React from "react";
+import "../Style/Mobile.css";
+import { FcCollapse } from "react-icons/fc";
+const Mobile = ({ isOpen, setIsOpen }) => {
   return (
-    <div>Mobile</div>
-  )
-}
+    <div className="mobile">
+      <div className="close_icon" onClick={()=>setIsOpen(!isOpen)}>
+      {/* <i class="fi fi-sr-cross-circle"></i> */}
+      <FcCollapse/>
+      </div>
+      <div className="mobile_options">
+      <div className="mobile_option">
+        <a href="#project">
+          <i class="fi fi-rr-edit-alt option_icon"></i>Projects
+        </a>
+      </div>
+      <div className="mobile_option">
+        <a href="#Skills">
+          <i class="fi fi-rr-laptop option_icon"></i>Skills
+        </a>
+      </div>
+      <div className="mobile_option">
+        <a href="#Work">
+          <i class="fi fi-rr-briefcase"></i>Work
+        </a>
+      </div>
+      <div className="mobile_option">
+        <a href="#Contact">
+          <i class="fi fi-rr-following"></i>Contact
+        </a>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default Mobile
+export default Mobile;
--- src/Utils/Web.jsx
+++ src/Utils/Web.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import "../Style/Web.css"
+import React from "react";
+import "../Style/Web.css";
+import "../Style/Web.css";
 
 const Web = () => {
   return (
-    <div>Web</div>
-  )
-}
+    <div className="web">
+      <div className="web_option">
+        <a href="#project">
+          <i class="fi fi-rr-edit-alt option_icon"></i>Projects
+        </a>
+      </div>
+      <div className="web_option">
+        <a href="#Skills">
+          <i class="fi fi-rr-laptop option_icon"></i>Skills
+        </a>
+      </div>
+      <div className="web_option">
+        <a href="#Work">
+          <i class="fi fi-rr-briefcase"></i>Work
+        </a>
+      </div>
+      <div className="web_option">
+        <a href="#Contact">
+          <i class="fi fi-rr-following"></i>Contact
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Web
+export default Web;
